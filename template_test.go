b82762bba17999352844c10f0e6d43dcb1ab48a6
neefrankie <neefrankie@gmail.com>
2019-09-29 00:05:44 +0800

    test

--- template_test.go
+++ template_test.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"gitlab.com/ftchinese/backyard/models"
 	"log"
 	"os"
 	"path/filepath"
 	"strings"
 	"testing"
 	"text/template"
+	"time"
 )
 
 const (
-	master = `Names: {{block "list" .}}{{"\n"}}{{range .}}{{println "-" .}}{{end}}{{end}}`
+	master  = `Names: {{block "list" .}}{{"\n"}}{{range .}}{{println "-" .}}{{end}}{{end}}`
 	overlay = `{{define "list"}} {{join . ","}}{{end}} `
 )
-func TestTemplate(t *testing.T)  {
+
+func TestFilePathGlob(t *testing.T) {
+	filenames, err := filepath.Glob("./views/**/*.html")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("File names: %+v", filenames)
+}
+
+func TestTemplate(t *testing.T) {
 	var (
 		funcs = template.FuncMap{
 			"join": strings.Join,
@@ -62,57 +74,37 @@ func TestGlobTemplate(t *testing.T) {
 	}
 }
 
-func parseTemplateDir() (*template.Template, error) {
-	dir, err := os.Getwd()
-	if err != nil {
-		return nil, err
-	}
-
-	pattern := filepath.Join(dir, "./views/**/*.html")
-
-	return template.ParseGlob(pattern)
-}
-
-func ParseTemplateDir(dir string) (*template.Template, error) {
-	var paths []string
-	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		if !info.IsDir() {
-			paths = append(paths, path)
-		}
-
-		return nil
- 	})
-
-	if err != nil {
-		return nil, err
-	}
-
-	return template.ParseFiles(paths...)
-}
-
-type TemplateDate struct {
-	SiteName string
-	SiteURL string
-}
-
-func TestTemplateToHTML(t *testing.T)  {
-	data := TemplateDate{
-		SiteName: "Theory and Practice",
-		SiteURL:  "http://www.ftchinese.com",
-	}
-
-	tmpl, err := ParseTemplateDir("views")
+func TestDirectory(t *testing.T) {
+	paths, err := GetAllFilePathsInDirectory("./")
 	if err != nil {
 		t.Error(err)
 	}
 
-	t.Logf("Tempalate name %s", tmpl.Name())
+	t.Logf("Current paths: %v", paths)
+}
 
-	for _, v := range tmpl.Templates()  {
+type TemplateData struct {
+	SiteName string
+	SiteURL  string
+}
+
+func TestTemplateToHTML(t *testing.T) {
+	data := models.Home{
+		SiteName:    "My Great Site",
+		SiteURL:     "https://github.com/neefrankie",
+		Footer:      models.Footer,
+		CurrentYear: time.Now().Year(),
+		Version:     "0.0.1",
+	}
+
+	tmpl, err := ParseDirectory("./views")
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Logf("Tempalete name %s", tmpl.Name())
+
+	for _, v := range tmpl.Templates() {
 		t.Logf("Template name %s", v.Name())
 	}
 
@@ -120,13 +112,4 @@ func TestTemplateToHTML(t *testing.T)  {
 	if err != nil {
 		t.Error(err)
 	}
-}
-
-func TestGlob(t *testing.T)  {
-	filenames, err := filepath.Glob("./views/**/*.html")
-	if err != nil {
-		t.Error(err)
-	}
-
-	t.Logf("File names: %+v", filenames)
 }

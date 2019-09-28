9a526cb8431a42d2792a8b84c71990465c95eed7
neefrankie <neefrankie@gmail.com>
2019-09-29 00:06:22 +0800

    feat: Parse a directory of template

--- main.go
+++ main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
+	"gitlab.com/ftchinese/backyard/models"
 	"html/template"
 	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 type User struct {
-	Name string `json:"name" xml:"name" form:"name" query:"name"`
+	Name  string `json:"name" xml:"name" form:"name" query:"name"`
 	Email string `json:"email" xml:"email" form:"email" query:"email"`
 }
 
@@ -21,10 +25,9 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
-
-func main()  {
+func main() {
 	t := &Template{
-		templates: template.Must(template.ParseGlob("views/*.html")),
+		templates: template.Must(ParseDirectory("./views")),
 	}
 
 	e := echo.New()
@@ -64,7 +67,7 @@ func main()  {
 	e.GET("/show", func(context echo.Context) error {
 		team := context.QueryParam("team")
 		member := context.QueryParam("member")
-		return context.String(http.StatusOK, "team: " + team + ", member: " + member)
+		return context.String(http.StatusOK, "team: "+team+", member: "+member)
 	})
 
 	// application/x-www-form-urlencoded
@@ -73,7 +76,7 @@ func main()  {
 	e.POST("/save", func(context echo.Context) error {
 		name := context.FormValue("name")
 		email := context.FormValue("email")
-		return context.String(http.StatusOK, "name: " + name + ", email:" + email)
+		return context.String(http.StatusOK, "name: "+name+", email:"+email)
 	})
 
 	g := e.Group("/admin")
@@ -84,11 +87,45 @@ func main()  {
 		return false, nil
 	}))
 
-
-
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
 func hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
+	data := models.Home{
+		SiteName:    "My Great Site",
+		SiteURL:     "https://github.com/neefrankie",
+		Footer:      models.Footer,
+		CurrentYear: time.Now().Year(),
+		Version:     "0.0.1",
+	}
+
+	return c.Render(http.StatusOK, "index.html", data)
+}
+
+// Recursively get all file paths in directory, including sub-directories.
+func GetAllFilePathsInDirectory(dirPath string) ([]string, error) {
+	var paths []string
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			paths = append(paths, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return paths, nil
+}
+
+// Recursively parse all files in directory, including sub-directories.
+func ParseDirectory(dirPath string) (*template.Template, error) {
+	paths, err := GetAllFilePathsInDirectory(dirPath)
+	if err != nil {
+		return nil, err
+	}
+	return template.ParseFiles(paths...)
 }

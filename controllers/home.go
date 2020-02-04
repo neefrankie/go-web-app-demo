0bf619403dc5735dfa77a57df68a8b977a4d6c80
neefrankie <neefrankie@gmail.com>
2020-02-04 12:04:26 +0800

    feat: try cookie

--- controllers/home.go
+++ controllers/home.go
@@ -3,8 +3,15 @@ package controllers
 import (
 	"github.com/labstack/echo/v4"
 	"net/http"
+	"time"
 )
 
 func GetHome(c echo.Context) error {
+	cookie := new(http.Cookie)
+	cookie.Name = "username"
+	cookie.Value = "jon"
+	cookie.Expires = time.Now().Add(24 * time.Hour)
+	c.SetCookie(cookie)
+
 	return c.Render(http.StatusOK, "home.html", nil)
 }
